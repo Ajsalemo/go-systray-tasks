ed8c9b7c50daaca7772651a5a73baf8e367a5616
Anthony Salemo <ajssalemo@gmail.com>
2024-07-17 21:52:24 -0400

    created a poc to copy/paste titles

--- go.mod
+++ go.mod
@@ -4,12 +4,25 @@ go 1.21.5
 
 require golang.design/x/clipboard v0.7.0
 
-require golang.design/x/mainthread v0.3.0 // indirect
+require (
+	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/gofiber/fiber/v2 v2.52.5 // indirect
+	github.com/google/uuid v1.5.0 // indirect
+	github.com/klauspost/compress v1.17.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.51.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.design/x/mainthread v0.3.0 // indirect
+)
 
 require (
 	golang.design/x/hotkey v0.4.1
 	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
 	golang.org/x/image v0.6.0 // indirect
 	golang.org/x/mobile v0.0.0-20230301163155-e0f57694e12c // indirect
-	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,12 +1,36 @@
 package main
 
 import (
+	"bytes"
 	"log"
+	"time"
 
+	"github.com/gofiber/fiber/v2"
 	"golang.design/x/clipboard"
 	"golang.design/x/hotkey"
-	"golang.design/x/hotkey/mainthread"
 )
+
+// TODO - clean this up to be more realistic. this is a POC
+func fn() {
+	pasteBacklogTitle := hotkey.New([]hotkey.Modifier{hotkey.ModCtrl, hotkey.ModShift}, hotkey.KeyS)
+	err := pasteBacklogTitle.Register()
+	if err != nil {
+		log.Fatalf("hotkey: failed to register hotkey: %v", err)
+		return
+	}
+	// Run this on a infinite loop to watch for key events
+	// Otherwise this doesn't watch further key events aside from the first one
+	for {
+		select {
+		case <-pasteBacklogTitle.Keydown():
+			log.Printf("hotkey: %v is down\n", pasteBacklogTitle)
+		case <-pasteBacklogTitle.Keyup():
+			backlogTitle := "test | backlog | " + time.Now().Format("2006/01/02")
+			clipboard.Write(clipboard.FmtText, bytes.NewBufferString(backlogTitle).Bytes())
+			log.Printf("hotkey: %v is up\n", pasteBacklogTitle)
+		}
+	}
+}
 
 func main() {
 	// Init returns an error if the package is not ready for use.
@@ -15,27 +39,16 @@ func main() {
 		panic(err)
 	}
 
-	mainthread.Init(fn)
-}
+	app := fiber.New()
+	// TODO - expose pprof endpoints for debugging/metrics to see if these for loops are going to cause a problem
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+	// Run these functions in a goroutine
+	// These keybind watchers are being watched on a infinite for loop. It seems better to run them in a goroutine because of this
+	go func() {
+		fn()
+	}()
 
-func fn() {
-	pasteBacklogTitle := hotkey.New([]hotkey.Modifier{hotkey.ModCtrl, hotkey.ModShift}, hotkey.KeyS)
-	err := pasteBacklogTitle.Register()
-	if err != nil {
-		log.Fatalf("hotkey: failed to register hotkey: %v", err)
-		return
-	}
-
-	log.Printf("hotkey: %v is registered\n", pasteBacklogTitle)
-	<-pasteBacklogTitle.Keydown()
-	log.Printf("hotkey: %v is down\n", pasteBacklogTitle)
-
-	if pasteBacklogTitle.Keydown() != nil {
-		log.Printf("hotkey: %v is down again \n", pasteBacklogTitle)
-	}
-
-	if pasteBacklogTitle.Keyup() != nil {
-		clipboard.Write(clipboard.FmtText, []byte("text data"))
-		log.Printf("hotkey: %v is up\n", pasteBacklogTitle)
-	}
+	log.Fatal(app.Listen(":3000"))
 }
