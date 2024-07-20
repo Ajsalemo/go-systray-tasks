c1973f663920e97ec8c6321d0abfc2d1095091cb
Anthony Salemo <ajssalemo@gmail.com>
2024-07-19 20:38:58 -0400

    replaced print statements with Zap - added all 4 main hotkeys

--- go.mod
+++ go.mod
@@ -16,6 +16,8 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
 	golang.design/x/mainthread v0.3.0 // indirect
 )
 
--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"bytes"
-	"log"
 	"os"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"go.uber.org/zap"
 	"golang.design/x/clipboard"
 	"golang.design/x/hotkey"
 )
@@ -15,51 +15,96 @@ import (
 func invokeHotKeys() {
 	pasteBacklogTitle := hotkey.New([]hotkey.Modifier{hotkey.ModCtrl}, hotkey.KeyS)
 	pasteBacklogBody := hotkey.New([]hotkey.Modifier{hotkey.ModCtrl, hotkey.ModShift}, hotkey.KeyS)
+	pasteAgedTitle := hotkey.New([]hotkey.Modifier{hotkey.ModCtrl}, hotkey.KeyD)
+	pasteAgedBody := hotkey.New([]hotkey.Modifier{hotkey.ModCtrl, hotkey.ModShift}, hotkey.KeyD)
 	// Register backlog title hotkey
-	err := pasteBacklogTitle.Register()
-	if err != nil {
-		log.Fatalf("hotkey: failed to register hotkey: %v", err)
+	errBacklogTitle := pasteBacklogTitle.Register()
+	if errBacklogTitle != nil {
+		zap.L().Error("hotkey: failed to register hotkey: CTRL + S")
+		zap.L().Fatal(errBacklogTitle.Error())
 		return
 	}
 	// Register backlog body hotkey
-	err = pasteBacklogBody.Register()
-	if err != nil {
-		log.Fatalf("hotkey: failed to register hotkey: %v", err)
+	errBacklogBody := pasteBacklogBody.Register()
+	if errBacklogBody != nil {
+		zap.L().Error("hotkey: failed to register hotkey: CTRL + SHIFT + S")
+		zap.L().Fatal(errBacklogBody.Error())
 		return
 	}
-	// Run this on a infinite loop to watch for key events
+	// Register aged title hotkey
+	errAgedTitle := pasteAgedTitle.Register()
+	if errAgedTitle != nil {
+		zap.L().Error("hotkey: failed to register hotkey: CTRL + D")
+		zap.L().Fatal(errAgedTitle.Error())
+		return
+	}
+	// Register aged body hotkey
+	errAgedBody := pasteAgedBody.Register()
+	if errAgedBody != nil {
+		zap.L().Error("hotkey: failed to register hotkey: CTRL + SHIFT + D")
+		zap.L().Fatal(errAgedBody.Error())
+		return
+	} // Run this on a infinite loop to watch for key events
 	// Otherwise this doesn't watch further key events aside from the first one
 	for {
 		select {
 		case <-pasteBacklogTitle.Keydown():
-			log.Printf("hotkey: %v is down\n", pasteBacklogTitle)
+			zap.L().Info("hotkey: CTRL + S is down")
 		// Create a new backlog title and paste it to the clipboard
 		case <-pasteBacklogTitle.Keyup():
 			backlogTitle := os.Getenv("BACKLOG_TITLE_PREFIX") + " | " + time.Now().Format("2006/01/02")
 			clipboard.Write(clipboard.FmtText, bytes.NewBufferString(backlogTitle).Bytes())
-			log.Printf("hotkey: %v is up\n", pasteBacklogTitle)
+			zap.L().Info("hotkey: CTRL + S is up")
 		case <-pasteBacklogBody.Keydown():
-			log.Printf("hotkey: %v is down\n", pasteBacklogBody)
+			zap.L().Info("hotkey: CTRL + SHIFT + S is down")
 		// Read the file and paste it to the clipboard for the backlog body
 		case <-pasteBacklogBody.Keyup():
 			backlogTitle := os.Getenv("BACKLOG_BODY_FILE_PATH")
-			content, err := os.ReadFile(backlogTitle)
+			backlogBodyContent, err := os.ReadFile(backlogTitle)
 			if err != nil {
-				log.Fatalf("failed to read file: %v", err)
+				zap.L().Error("failed to read file for BACKLOG_BODY_FILE_PATH")
+				zap.L().Error("is the filesytem read-only or does the file exist?")
+				zap.L().Fatal(err.Error())
 				return
 			}
 
-			clipboard.Write(clipboard.FmtText, bytes.NewBufferString(string(content)).Bytes())
-			log.Printf("hotkey: %v is up\n", pasteBacklogBody)
+			clipboard.Write(clipboard.FmtText, bytes.NewBufferString(string(backlogBodyContent)).Bytes())
+			zap.L().Info("hotkey: CTRL + SHIFT + S is up")
+		case <-pasteAgedTitle.Keydown():
+			zap.L().Info("hotkey: CTRL + D is down")
+		// Create a new backlog title and paste it to the clipboard
+		case <-pasteAgedTitle.Keyup():
+			agedTitle := os.Getenv("AGED_TITLE_PREFIX") + " | " + time.Now().Format("2006/01/02")
+			clipboard.Write(clipboard.FmtText, bytes.NewBufferString(agedTitle).Bytes())
+			zap.L().Info("hotkey: CTRL + D is up")
+		case <-pasteAgedBody.Keydown():
+			zap.L().Info("hotkey: CTRL + SHIFT + D is down")
+		// Read the file and paste it to the clipboard for the aged body
+		case <-pasteAgedBody.Keyup():
+			agedTitle := os.Getenv("AGED_BODY_FILE_PATH")
+			agedBodyContent, err := os.ReadFile(agedTitle)
+			if err != nil {
+				zap.L().Error("failed to read file for AGED_BODY_FILE_PATH")
+				zap.L().Error("is the filesytem read-only or does the file exist?")
+				zap.L().Fatal(err.Error())
+				return
+			}
+
+			clipboard.Write(clipboard.FmtText, bytes.NewBufferString(string(agedBodyContent)).Bytes())
+			zap.L().Info("hotkey: CTRL + SHIFT + D is up")
 		}
 	}
+}
+
+func init() {
+	zap.ReplaceGlobals(zap.Must(zap.NewProduction()))
 }
 
 func main() {
 	// Init returns an error if the package is not ready for use.
 	err := clipboard.Init()
 	if err != nil {
-		panic(err)
+		zap.L().Fatal(err.Error())
 	}
 
 	app := fiber.New()
@@ -73,5 +118,9 @@ func main() {
 		invokeHotKeys()
 	}()
 
-	log.Fatal(app.Listen(":3000"))
+	fiberErr := app.Listen(":3000")
+
+	if fiberErr != nil {
+		zap.L().Fatal(fiberErr.Error())
+	}
 }
